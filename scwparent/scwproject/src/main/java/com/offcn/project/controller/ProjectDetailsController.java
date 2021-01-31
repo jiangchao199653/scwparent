47768eb4b07be387fc9d3e854d8c657a066137db
offcn-java <myqq@qq.com>
2021-01-31 16:39:30 +0800

    跟新

--- scwparent/scwproject/src/main/java/com/offcn/project/controller/ProjectDetailsController.java
+++ scwparent/scwproject/src/main/java/com/offcn/project/controller/ProjectDetailsController.java
@@ -6,6 +6,7 @@ import com.offcn.project.po.TReturn;
 import com.offcn.project.service.ProjectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import net.minidev.json.JSONUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +20,11 @@ public class ProjectDetailsController {
     @Autowired
     private ProjectService projectService;
 
+
     @ApiOperation("获取回报详情")
     @GetMapping("/getReturnByRid")
     public AppResponse<TReturn> getReturnByRid(Integer rid){
+        System.out.println("123");
         TReturn tReturn = projectService.getReturnByRid(rid);
         return AppResponse.ok(tReturn);
     }
